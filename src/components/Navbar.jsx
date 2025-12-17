c8c8ca68fa092e6470814438f19b3762369bf530
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-18 00:32:45 +0500

    ai chat via grok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import { EllipsisVertical } from 'lucide-react'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <div className="">
+     <div className='flex items-center justify-between bg-gray-900 text-white sticky z-50 py-2 px-4 sm:px-6 md:px-8 lg:px-10 rounded-b-md'>
+        <img src="./image.png" alt="profile" className='w-10 h-10 rounded-full object-cover' />
+        <EllipsisVertical className='cursor-pointer text-gray-500 hover:text-gray-300 transition duration-300' />
+    </div>
+   </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Send } from 'lucide-react'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
+    <div className='w-full h-full flex flex-col px-2'>
+      <Navbar />
+
+    <div className="h-full bg-gray-900 text-white rounded-lg my-2">a</div>
+
+      <div className="mt-auto flex items-center gap-3 bg-gray-900 text-white my-2 p-4 rounded-full">
+        <button className='bg-blue-500 text-white rounded-full shadow cursor-pointer p-2'>
+          <Send size={18} />
+        </button>
+        <input type="text" name="search" id="search" placeholder='Search' 
+        className='w-full p-2 outline-none bg-gray-800 text-white rounded-full' />
+      </div>
     </div>
   )
 }
