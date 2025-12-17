91cfe55b5f4c0a54a8108a93b879db60dcc2f3b9
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-17 21:44:56 +0500

    ai chat via grok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './layout/Layout'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={<Layout/>} >
+        <Route index element={<Home/>} />
+      </Route>
+    </Routes>
+    </>
   )
 }
 
